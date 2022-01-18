73721fb936901c23d793d263bc7742140800fdaf
hiimivantang <hiimivantang>
2022-01-18 02:59:18 +0000

    added mount s3 example

---
+++ mount-s3-buckets/mount-bucket.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+access_key = dbutils.secrets.get(scope = "aws_storage_keys", key = "aws-access-key")
+secret_key = dbutils.secrets.get(scope = "aws_storage_keys", key = "aws-secret-key")
+
+# COMMAND ----------
+
+aws_bucket_name = "hiimivantang-canedge"
+mount_name = "canedge2"
+
+dbutils.fs.mount("s3a://%s:%s@%s" % (access_key, secret_key.replace("/", "%2F"), aws_bucket_name), "/mnt/%s" % mount_name)
+display(dbutils.fs.ls("/mnt/%s" % mount_name))
+
